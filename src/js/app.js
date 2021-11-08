f238904ee0ac3072ba3d2699fa11522b6fc5b7da
igor-chazov <igor-chazov@yandex.ru>
2021-11-08 23:00:47 +0500

    Исправлен URL

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import API from './Rest';
 import Chat from './Chat';
 import PageController from './PageController';
 
-const api = new API('http://sse-ws-chat-backend.herokuapp.com/contacts');
+const api = new API('https://sse-ws-chat-backend.herokuapp.com/contacts');
 const chat = new Chat(document.querySelector('.container'));
 const pageCtrl = new PageController(api, chat);
 pageCtrl.bindToDOM(document.querySelector('.container'));
